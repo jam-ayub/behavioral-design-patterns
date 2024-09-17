36b23c270bda956c2bb34ca26d42e01ecbe723f6
Jam <jamayub098@gmail.com>
2024-09-18 00:21:27 +0500

    iterator pattern implementation with Array

--- src/iterator/BrowseHistory.java
+++ src/iterator/BrowseHistory.java
@@ -1,44 +1,39 @@
 package iterator;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class BrowseHistory {
-    private List<String> urls = new ArrayList<>();
+    private String[] urls = new String[10];
+    private int count = 0;
 
     public void push(String url) {
-        urls.add(url);
+        urls[count] = url;
+        count++;
     }
 
     public String pop() {
-        var lastIdx = urls.size() - 1;
-        var lastUrl = urls.get(lastIdx);
-        urls.remove(lastUrl);
-
-        return lastUrl;
+        return urls[--count];
     }
 
     public Iterator createIterator() {
-        return new ListIterator(this);
+        return new Arrayterator(this);
     }
 
-    public class ListIterator implements Iterator {
+    public class Arrayterator implements Iterator {
 
         private final BrowseHistory history;
         private int index;
 
-        public ListIterator(BrowseHistory history) {
+        public Arrayterator(BrowseHistory history) {
             this.history = history;
         }
 
         @Override
         public boolean hasNext() {
-            return (index < history.urls.size());
+            return (index < history.count);
         }
 
         @Override
         public String current() {
-            return history.urls.get(index);
+            return history.urls[index];
         }
 
         @Override
