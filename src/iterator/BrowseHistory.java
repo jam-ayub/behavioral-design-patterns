c5d9f0667403af8ae777a2a6c55c51666a524737
Jam <jamayub098@gmail.com>
2024-09-17 13:58:43 +0500

    iterator pattern implementation with List of string

---
+++ src/iterator/BrowseHistory.java
@@ -0,0 +1,49 @@
+package iterator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrowseHistory {
+    private List<String> urls = new ArrayList<>();
+
+    public void push(String url) {
+        urls.add(url);
+    }
+
+    public String pop() {
+        var lastIdx = urls.size() - 1;
+        var lastUrl = urls.get(lastIdx);
+        urls.remove(lastUrl);
+
+        return lastUrl;
+    }
+
+    public Iterator createIterator() {
+        return new ListIterator(this);
+    }
+
+    public class ListIterator implements Iterator {
+
+        private final BrowseHistory history;
+        private int index;
+
+        public ListIterator(BrowseHistory history) {
+            this.history = history;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (index < history.urls.size());
+        }
+
+        @Override
+        public String current() {
+            return history.urls.get(index);
+        }
+
+        @Override
+        public void next() {
+            index++;
+        }
+    }
+}
